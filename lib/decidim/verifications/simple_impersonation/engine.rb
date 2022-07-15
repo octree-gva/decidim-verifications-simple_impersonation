a6c4c4a7dbe98fb67c2d6e3f3b38b03fc18c837e
Hadrien Froger <hadrien@octree.ch>
2022-07-15 09:27:36 -0300

    chore: remove useless routes

--- lib/decidim/verifications/simple_impersonation/engine.rb
+++ lib/decidim/verifications/simple_impersonation/engine.rb
@@ -11,15 +11,11 @@ module Decidim
       # This is the engine that runs on the public interface of simple impersonation.
       class Engine < ::Rails::Engine
         isolate_namespace Decidim::Verifications::SimpleImpersonation
-        routes do
-          resource :authorization, only: [:new], as: :authorization
-          root to: "authorizations#new"
-        end
-        initializer "decidim_module_simple_impersonation.assets" do |app|
+        initializer "decidim_verifications_simple_impersonation.assets" do |app|
           app.config.assets.paths << File.expand_path("#{Decidim::Verifications::SimpleImpersonation::Engine.root}/app/assets")
         end
 
-        initializer "decidim_module_simple_impersonation.add_cells_view_paths" do |app|
+        initializer "decidim_verifications_simple_impersonation.add_cells_view_paths" do |app|
           Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Verifications::SimpleImpersonation::Engine.root}/app/views")
         end
       end
